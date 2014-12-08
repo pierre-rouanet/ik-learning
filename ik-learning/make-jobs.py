f041cb744dcf43cfe74bea5c6ad63494795b05e3
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-12-08 16:54:46 +0100

    Add a script to launch all jobs.

---
+++ ik-learning/make-jobs.py
@@ -0,0 +1,34 @@
+SM = ('knn', 'ilo-gmm', 'imle')
+IM = ('random', 'discretized_progress')
+BABBLING = ('motor', 'goal')
+N = 25
+
+pbs = """
+#!/bin/sh
+
+#PBS -o logs/output/{log}.output
+#PBS -e logs/error/{log}.error
+
+cd xp/ik-learning
+python ik.py --interest-model {im} --babbling-mode {bab} --sensorimotor-model {sm} --iteration {i}
+
+"""
+
+from subprocess import call
+
+
+if __name__ == '__main__':
+    for im in IM:
+        for bab in BABBLING:
+            for sm in SM:
+                if ((im == 'discretized_progress' and bab == 'motor') or (sm == 'imle')):
+                    print 'Skipping', im, bab, sm
+                    continue
+
+                for i in range(1, N + 1):
+                    log = 'ik-xp-{}-{}-with-{}-{}'.format(im, bab, sm, i)
+
+                    with open('/tmp/ik.pbs', 'w') as f:
+                        f.write(pbs.format(log=log, im=im, bab=bab, sm=sm, i=i))
+
+                        call(["qsub", "/tmp/ik.pbs"])
