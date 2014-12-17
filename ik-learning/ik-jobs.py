87f6aae705283729c2fa8bf3a2e520665e67d09c
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-12-17 16:16:05 +0100

    Update the way we send parameters to ik (using conf)

--- ik-learning/ik-jobs.py
+++ ik-learning/ik-jobs.py
@@ -1,18 +1,29 @@
-SM = ('knn', 'ilo-gmm', 'imle')
-IM = ('random', 'discretized_progress')
-BABBLING = ('motor', 'goal')
-N = 100
+import os
+
+xp_conf = {
+    'scene': 'poppy-flying.ttt',
+
+    'bab': 'motor',
+    'im': {'name': 'random', 'conf': {}},
+    'sm': {'name': 'knn', 'conf': {'sigma_ratio': 1 / 6.}},
+
+    'eval_at': [5, 10, 20, 30, 40, 50, 100, 150,
+                200, 250, 300, 400, 500, 600, 700, 800, 900, 1000,
+                1250, 1500, 1750, 2000, 3000, 4000, 5000],
+    'tc': 'tc-150.npy',
+
+    'log_folder': 'logs/gridsearch',
+    'log': 'random-motor-knn-default-conf',
+}
 
 pbs = """
 #!/bin/sh
 
-#PBS -o logs/grid/std/{log}.output
-#PBS -e logs/grid/std/{log}.error
-#PBS -l walltime=6:00:0
-#PBS -N ik-learning-{im}-{bab}-{sm}-{i}
+#PBS -l walltime=10:00:0
+#PBS -N rm-knn-{i}
 
 cd xp/ik-learning/ik-learning
-python ik.py --interest-model {im} --babbling-mode {bab} --sensorimotor-model {sm} --iteration {i}
+python ik.py -c {config} -i {i}
 
 """
 
@@ -20,17 +31,37 @@ from subprocess import call
 
 
 if __name__ == '__main__':
-    for im in IM:
-        for bab in BABBLING:
-            for sm in SM:
-                if ((im == 'discretized_progress' and bab == 'motor') or (sm == 'imle')):
-                    print 'Skipping', im, bab, sm
-                    continue
+    avakas = 'AVAKAS' in os.environ
 
-                for i in range(1, N + 1):
-                    log = 'ik-xp-{}-{}-with-{}-{}'.format(im, bab, sm, i)
+    conf = '/tmp/xp.conf'
+    with open(conf, 'w') as f:
+        f.write(str(xp_conf))
 
-                    with open('/tmp/ik.pbs', 'w') as f:
-                        f.write(pbs.format(log=log, im=im, bab=bab, sm=sm, i=i))
+    stop
 
-                    call(["qsub", "/tmp/ik.pbs"])
+    for i in range(100):
+        if avakas:
+            with open('/tmp/ik.pbs', 'w') as f:
+                f.write(pbs.format(config=conf, i=i))
+
+            call(["qsub", "/tmp/ik.pbs"])
+
+        else:
+            call(["python", "ik.py", "-c", conf, "-i", str(i)])
+
+
+
+    # for im in IM:
+    #     for bab in BABBLING:
+    #         for sm in SM:
+    #             if ((im == 'discretized_progress' and bab == 'motor') or (sm == 'imle')):
+    #                 print 'Skipping', im, bab, sm
+    #                 continue
+    #
+    #             for i in range(1, N + 1):
+    #                 log = 'ik-xp-{}-{}-with-{}-{}'.format(im, bab, sm, i)
+    #
+    #                 with open('/tmp/ik.pbs', 'w') as f:
+    #                     f.write(pbs.format(log=log, im=im, bab=bab, sm=sm, i=i))
+    #
+    #                 call(["qsub", "/tmp/ik.pbs"])
--- ik-learning/ik.py
+++ ik-learning/ik.py
@@ -8,27 +8,24 @@ from numpy import array, load
 from pyvrep.xp import VrepXp
 
 from explauto.agent import Agent
-from explauto import InterestModel
 from explauto.utils import bounds_min_max
 from explauto.experiment import Experiment
+from explauto.interest_model import interest_models
 # from explauto.sensorimotor_model.imle import ImleModel
 from explauto.sensorimotor_model.ilo_gmm import IloGmm
 from explauto.environment.environment import Environment
 from explauto.sensorimotor_model.nearest_neighbor import NearestNeighbor
 
-log_folder = 'logs/tc'
-scene = 'poppy-flying.ttt'
 
 sms = {
-    'knn': (NearestNeighbor, {'sigma_ratio': 1. / 16}),
-    # 'imle': (ImleModel, {}),
-    'ilo-gmm': (IloGmm, {}),
+    'knn': NearestNeighbor,
+    'ilo-gmm': IloGmm,
 }
 
 motors = ['l_hip_x', 'l_hip_z', 'l_hip_y', 'l_knee_y', 'l_ankle_y']
 alias = 'l_leg'
 
-conf = {
+env_conf = {
     'motors': alias,
     'move_duration': 1.,
     'm_mins': array([-180.] * len(motors)),
@@ -36,15 +33,6 @@ conf = {
     's_mins': array([-.5, -.5, -.5]),
     's_maxs': array([.5, .5, .5]),
 }
-
-# eval_at = [5, 10]
-eval_at = [5, 10, 20, 30, 40, 50, 100, 150,
-           200, 250, 300, 400, 500, 600, 700, 800, 900, 1000,
-           1250, 1500, 1750, 2000]
-
-# tc = load('tc-grid.npy')
-tc = load('tc-150.npy')
-# tc = load('tc-grid.npy')[:3]
 
 avakas = 'AVAKAS' in os.environ
 
@@ -79,24 +67,28 @@ class VrepEnvironment(Environment):
 
 
 class LearningIkXp(VrepXp):
-    def __init__(self, im, bab, sm):
-        VrepXp.__init__(self, 'poppy', scene)
+    def __init__(self, conf):
+        VrepXp.__init__(self, 'poppy', conf['scene'])
 
-        self.im_name, self.babbling, self.sm_name = im, bab, sm
+        self.xp_conf = conf
 
     def run(self):
-        env = VrepEnvironment(self.robot, **conf)
+        env = VrepEnvironment(self.robot, **env_conf)
 
-        im_dims = env.conf.m_dims if self.babbling == 'motor' else env.conf.s_dims
-        im = InterestModel.from_configuration(env.conf, im_dims, self.im_name)
+        # Create the Interest Model
+        im_dims = env.conf.m_dims if self.xp_conf['bab'] == 'motor' else env.conf.s_dims
+        im_cls, _ = interest_models[self.xp_conf['im']['name']]
+        im_conf = self.xp_conf['im']['conf']
+        im = im_cls(env.conf, im_dims, **im_conf)
 
-        sm_cls, kwargs = sms[self.sm_name]
-        sm = sm_cls(env.conf, **kwargs)
+        # Create the SensoriMotor Model
+        sm_cls = sms[self.xp_conf['sm']['name']]
+        sm = sm_cls(env.conf, **self.xp_conf['sm']['conf'])
 
         ag = Agent(env.conf, sm, im)
 
         self.xp = Experiment(env, ag)
-        self.xp.evaluate_at(eval_at, tc)
+        self.xp.evaluate_at(self.xp_conf['eval_at'], load(self.xp_conf['tc']))
         self.xp.run()
 
     def save(self, f):
@@ -105,21 +97,17 @@ class LearningIkXp(VrepXp):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--interest-model', type=str, required=True,
-                        choices=('random', 'discretized_progress'))
 
-    parser.add_argument('--babbling-mode', type=str, required=True,
-                        choices=('motor', 'goal'))
-
-    parser.add_argument('--sensorimotor-model', type=str, required=True,
-                        choices=('knn', 'ilo-gmm', 'imle'))
-
-    parser.add_argument('--iteration', type=int, required=True)
+    parser.add_argument('-c', '--config', type=str, required=True)
+    parser.add_argument('-i', '--iteration', type=int, required=True)
 
     args = parser.parse_args()
 
-    log = 'ik-xp-{}-{}-with-{}-{}.logs'.format(args.interest_model, args.babbling_mode,
-                                               args.sensorimotor_model, args.iteration)
+    with open(args.config) as f:
+        xp_conf = eval(f.read())
+
+    log = '{}-{}.logs'.format(xp_conf['log'], args.iteration)
+    log_folder = xp_conf['log_folder']
 
     log_file = os.path.join(log_folder, log)
 
@@ -129,13 +117,13 @@ if __name__ == '__main__':
     if os.path.exists(log_file):
         raise ValueError('Log already exists ({})!'.format(log_file))
 
-    xp = LearningIkXp(args.interest_model,
-                      args.babbling_mode,
-                      args.sensorimotor_model)
+    xp = LearningIkXp(xp_conf)
 
     t0 = time.time()
+
     if avakas:
         xp.spawn(log_file, avakas=True)
     else:
         xp.spawn(log_file, gui=True)
+
     print 'Ellapsed time', time.time() - t0
