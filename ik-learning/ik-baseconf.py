7b42ba34fb3d39d06367e1f66a1e341224e63fde
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-12-17 16:16:55 +0100

    Add config examples

---
+++ ik-learning/ik-baseconf.py
@@ -0,0 +1,15 @@
+{
+    'scene': 'poppy-flying.ttt',
+
+    'bab': 'motor',
+    'im': {'conf': {}, 'name': 'random'},
+    'sm': {'conf': {'sigma_ratio': 0.25}, 'name': 'knn'},
+
+    'eval_at': [5, 10, 20, 30, 40, 50, 100, 150,
+                200, 250, 300, 400, 500, 600, 700, 800, 900, 1000,
+                1250, 1500, 1750, 2000, 3000, 4000, 5000],
+    'tc': 'tc-150.npy',
+
+    'log_folder': 'logs/tmp',
+    'log': 'salut',
+}
---
+++ ik-learning/ik-dummyconf.py
@@ -0,0 +1,13 @@
+{
+    'scene': 'poppy-flying.ttt',
+
+    'bab': 'motor',
+    'im': {'name': 'random', 'conf': {}},
+    'sm': {'name': 'knn', 'conf': {'sigma_ratio': 1 / 6.}},
+
+    'eval_at': [5, 10],
+    'tc': 'tc-3.npy',
+
+    'log_folder': 'logs/tmp',
+    'log': 'salut',
+}
