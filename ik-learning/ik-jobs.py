d02d1e5c2f6e76b5873145f24c16ce6a080fac53
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-12-20 11:09:18 +0100

    Up

--- ik-learning/ik-jobs.py
+++ ik-learning/ik-jobs.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
     avakas = 'AVAKAS' in os.environ
 
-    xp_name = 'rm-knn'
+    xp_name = 'dpg-knn'
 
     with open('config/{}.py'.format(xp_name)) as f:
         xp_conf = eval(f.read())
@@ -36,16 +36,23 @@ if __name__ == '__main__':
         xp_conf['tc'] = 'tc-3.npy'
         N = 2
 
-    conf = os.path.join(os.getcwd(), 'tmp', 'xp-{}.conf'.format(xp_name))
-    with open(conf, 'w') as f:
-        f.write(str(xp_conf))
+    for x_card in map(lambda x: x ** 3, [2, 3, 5, 7, 10, 16]):
+        for win_size in [5, 10, 20, 50, 75, 100, 200]:
+            name = '{}-xcard-{}-winsize-{}'.format(xp_name, x_card, win_size)
+            xp_conf['log'] = name
+            xp_conf['im']['conf'] = {'x_card': x_card, 'win_size': win_size}
+            print 'Starting {} xp using {}'.format(N, xp_conf)
 
-    for i in range(N):
-        if (not args.test) and avakas:
-            with open('/tmp/ik.pbs', 'w') as f:
-                f.write(pbs.format(name=xp_name, config=conf, i=i))
+            conf = os.path.join(os.getcwd(), 'tmp', 'xp-{}.conf'.format(name))
+            with open(conf, 'w') as f:
+                f.write(str(xp_conf))
 
-            call(["qsub", "/tmp/ik.pbs"])
+            for i in range(N):
+                if (not args.test) and avakas:
+                    with open('/tmp/ik.pbs', 'w') as f:
+                        f.write(pbs.format(name=xp_name, config=conf, i=i + 1))
 
-        else:
-            call(["python", "ik.py", "-c", conf, "-i", str(i)])
+                    call(["qsub", "/tmp/ik.pbs"])
+
+                else:
+                    call(["python", "ik.py", "-c", conf, "-i", str(i)])
